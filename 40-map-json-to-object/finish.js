9c18b81b2971d0af91fec84940a337b469f0d447
Андрей <andrej@MacBook-Air.local>
2022-12-26 21:08:32 +0300

    feat:solution tasks 35-40

--- 40-map-json-to-object/finish.js
+++ 40-map-json-to-object/finish.js
@@ -14,4 +14,10 @@ const postsJSON = [
   '{"postId":5131,"commentsQuantity":13}',
   '{"postId":6134,"commentsQuantity":2}',
   '{"postId":2351,"commentsQuantity":8}',
-]
+];
+
+const parsePostsJSON = postsJSON.map((post) => JSON.parse(post));
+
+console.log(parsePostsJSON);
+console.log(parsePostsJSON[1].postId);
+console.log(parsePostsJSON.at(-1).commentsQuantity);
